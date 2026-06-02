6f172db0bbc861cd16190caabb1d3e0ee1338838
successdevop <successraphael28@gmail.com>
2026-06-02 19:17:36 +0100

    revisited path parameters and their usefulness

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,56 @@
+from typing import Any
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 
 
 app = FastAPI()
 
-@app.get("/")
-def get_shipment():
-    return {
-        "content":"wooden table",
-        "status":"in transit"
-    }
+shipment_db = {
+
+        123:{
+            "id": 12789,
+            "weight": 0.6,
+            "content": "glassware",
+            "status": "placed"
+        },
+        124:{
+            "id": 12790,
+            "weight": 0.8,
+            "content": "ceramics",
+            "status": "placed"
+        },
+        125:{
+            "id": 12791,
+            "weight": 1.2,
+            "content": "electronics",
+            "status": "shipped"
+        },
+        126:{
+            "id": 12792,
+            "weight": 0.5,
+            "content": "textiles",
+            "status": "pending"
+        },
+        127:{
+            "id": 12793,
+            "weight": 2.0,
+            "content": "furniture",
+            "status": "delivered"
+        }
+}
+
+
+@app.get("/shipment/latest")
+def get_latest_shipment() -> dict[str, Any]:
+    id = max(shipment_db.keys())
+    return shipment_db[id]
+
+
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str, Any]:
+    if id not in shipment_db:
+        return {"message":f"shipment with id {id} not found"}
+    return shipment_db[id]
 
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
--- app/main.py
+++ app/main.py
@@ -13,3 +13,21 @@
 #
 # root25 = root(25)
 # print(root25)
+from typing import Optional
+
+
+# def fence(val: str = "+"):
+#     def inner_fence(func):
+#         def wrapper_func():
+#             print(val*10)
+#             func()
+#             print(val*10)
+#         return wrapper_func
+#     return inner_fence
+#
+#
+# @fence
+# def log():
+#     print("decorated!")
+#
+# log()
